ed6e420be126a2a045c988c997b411e826b0d8f7
Gurshaan <138469341+GS-109@users.noreply.github.com>
2026-05-18 12:22:34 +0000

    Budget tracker

---
+++ budget-tracker/main.py
@@ -0,0 +1,14 @@
+transaction = []
+
+print("\n===Budget Tracker===")
+print("Options:\n1. Transaction\n2. Leave")
+choice = input("What option would you like to proceed with: ").lower()
+
+if choice = "1" or "transaction":
+    print("You are being redirected to the transaction page.")
+else:
+    choice2 = input("Are you sure you want to leave (Y/N): ").lower()
+if choice2 = "y":
+    print("You are about to leave!")
+else:
+    print("You will stay in the program!")
